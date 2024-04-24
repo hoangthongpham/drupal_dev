2071938941f66e533b48a1306da68ffbdff7d6d1
hoangthong <hoangthongpham@outlook.com>
2024-04-24 15:24:57 +0700

    add theme mini_store

---
+++ src/themes/mini_store/js/mini-store.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * mini_store behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.miniStore = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
